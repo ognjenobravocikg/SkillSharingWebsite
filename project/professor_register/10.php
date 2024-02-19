d68929704c9b25963c790b935a2af7ef59922e1b
mirkaa1303 <109366249+mirkaa1303@users.noreply.github.com>
2024-02-19 17:18:37 +0100

    Add files via upload

---
+++ project/professor_register/10.php
@@ -0,0 +1,76 @@
+<?php
+session_start();
+
+include("connection.php");
+include("functions.php");
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if (!empty($email) && !empty($password)) {
+
+        // Read from the database
+        $query = "select * from professors where email = '$email' limit 1";
+        $result = mysqli_query($con, $query);
+
+        if ($result) {
+            if ($result && mysqli_num_rows($result) > 0) {
+
+                $user_data = mysqli_fetch_assoc($result);
+
+                if ($user_data['password'] === $password) {
+
+                    $_SESSION['professor_id'] = $user_data['professor_id'];
+                    header("Location: http://localhost/testovi/project/professor_index.php");
+                    die;
+                }
+            }
+        }
+
+        echo "wrong username or password!";
+    } else {
+        echo "wrong username or password!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,
+    initial-scale=1.0" >
+    <title>Prijava</title>
+    <link rel="stylesheet" href="style3.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'> 
+</head>
+
+<body>
+    <div class="wrapper">
+        <h1>Prijava</h1>
+        <form method="post">
+            <div class="input-box">
+                <input type="email" name="email" placeholder="E-mail" required>
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+                <input type="password" name="password" placeholder="Šifra" required>
+                <i class='bx bxs-lock-alt' ></i>
+            </div>
+            <div class="remember-forgot">
+                <label><input type="checkbox" name="remember"> Zapamti me</label>
+                
+            </div>
+            <input class="btn" type="submit" value="Prijavi se">
+            <div class="register-link">
+                <p><a href="http://localhost/testovi/project/professor_register/login.php">Prijavi se kao profesor!</a></p>
+                <p><a href="signup.php">Nemaš nalog? Registruj se!</a></p>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
