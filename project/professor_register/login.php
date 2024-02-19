1abac7860db70f21635dd5950829cafd73337a36
Ognjen Obradović <94371789+ognjenobravocikg@users.noreply.github.com>
2024-02-19 23:56:39 +0100

    Add files via upload

--- project/professor_register/login.php
+++ project/professor_register/login.php
@@ -49,20 +49,19 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prijava</title>
-    <link rel="stylesheet" type="text/css" href="professor_style/login_style.css">
+    <title>Profesor prijava</title>
+    <link rel="stylesheet" href="professor_style/login_style.css">
 </head>
 <body>
     <div id="box">
         <form method="post">
-            <div>Prijava</div>
+            <h1>Prijavljujete se kao profesor</h1>
             <input type="email" name="email" placeholder="E-mail" required><br><br>
             <input type="password" name="password" placeholder="Šifra" required><br><br>
             <input id="button" type="submit" value="Uloguj se"><br><br>
-            <a href="http://localhost/testovi/project/user_register/login.php">Prijavi se kao učenik!</a><br><br>
-            <a href="signup.php">Nemaš nalog? Registruj se!</a>
+            <a href="http://localhost/testovi/project/user_register/login.php">Prijavi se kao učenik!</a><br>
+            <a href="signup.php">Nemate nalog? Registruj se!</a>
         </form>
     </div>
 </body>
 </html>
-
--- project/user_register/login.php
+++ project/user_register/login.php
@@ -49,20 +49,37 @@ session_start();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,
+    initial-scale=1.0" >
     <title>Prijava</title>
-	<link rel="stylesheet" href="user_register_style/login_style.css">
+    <link rel="stylesheet" href="user_register_style/login_style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'> 
 </head>
+
 <body>
-    <div id="box">
+    <div class="wrapper">
+        <h1>Prijava</h1>
         <form method="post">
-            <div>Prijava</div>
-            <input type="email" name="email" placeholder="E-mail" required><br><br>
-            <input type="password" name="password" placeholder="Šifra" required><br><br>
-            <input id="button" type="submit" value="Uloguj se"><br><br>
-			<a href="http://localhost/testovi/project/professor_register/login.php">Prijavi se kao profesor!</a><br><br>            
-			<a href="signup.php">Nemaš nalog? Registruj se!</a>
+            <div class="input-box">
+                <input type="email" name="email" placeholder="E-mail" required>
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+                <input type="password" name="password" placeholder="Šifra" required>
+                <i class='bx bxs-lock-alt' ></i>
+            </div>
+            <div class="remember-forgot">
+                <label><input type="checkbox" name="remember"> Zapamti me</label>
+                
+            </div>
+            <input class="btn" type="submit" value="Prijavi se"><br>
+            <div class="register-link">
+                <p><a href="http://localhost/testovi/project/professor_register/login.php">Prijavi se kao profesor!</a></p><br>
+                <p><a href="signup.php">Nemaš nalog? Registruj se!</a></p>
+            </div>
         </form>
     </div>
 </body>
+
 </html>
